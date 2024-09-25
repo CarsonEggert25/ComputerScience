0aa861a90f66ee24b788295b68709f9db18b08d5
CarsonEggert25 <832218@mystma.org>
2024-09-25 09:31:42 -0500

    Animal Survey plus comments

--- Assignments/animalsurvey.py
+++ Assignments/animalsurvey.py
@@ -1,9 +1,5 @@
-user_input1 = input("whats your favorite animal?\n>")
-print(user_input1)
-user_input2 = input("why is that your favorite animal?\n>")
-print(user_input2)
-user_input3 = input("where do they live?\n>")
-print(user_input3)
-user_input4 = input("what is a fun fact about that animal?\n>")
-print(user_input4)
-print("your favorite animal is " + user_input1 + " because " + user_input2 + " they live in " + user_input3 + " and " + user_input4)
+user_input1 = input("whats your favorite animal?\n>")       #ask what their favorite animal is
+user_input2 = input("why is that your favorite animal?\n>")     #ask them why that animal is their favorite
+user_input3 = input("where do they live?\n>")           #ask what part of the world their favorite animal lives
+user_input4 = input("what is a fun fact about that animal?\n>")     #ask them a fun fact about the animal
+print("your favorite animal is " + user_input1 + " because " + user_input2 + " they live in " + user_input3 + " and " + user_input4)    #a summary of the data collected
