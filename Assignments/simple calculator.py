fb0506567ee9865e44bd325a3d2826c69686e4da
CarsonEggert25 <832218@mystma.org>
2024-10-01 08:51:07 -0500

    simple calc

---
+++ Assignments/simple calculator.py
@@ -0,0 +1,39 @@
+def add():
+   print("add two numbers:")
+   x = (input("what is the first number?\n>"))
+   x = int(x)
+   y = (input("what is the second number?\n>"))
+   y = int(y)
+   print(str(x) + " + " + str(y) + " = " + str(x + y))
+
+add()
+     
+def subtract():
+   print("subtract two numbers:")
+   x = (input("what us the first number?\n>"))
+   x = int(x)
+   y = (input("what is the second number?\n>"))
+   y = int(y)
+   print(str(x) + " - " + str(y) + " = " + str(x - y))
+  
+subtract()
+
+def multiply():
+   print("multiply two numbers:")
+   x = (input("what us the first number?\n>"))
+   x = int(x)
+   y = (input("what is the second number?\n>"))
+   y = int(y)
+   print(str(x) + " * " + str(y) + " = " + str(x * y))
+  
+multiply() 
+
+def divide():
+   print("divide two numbers:")
+   x = (input("what us the first number?\n>"))
+   x = int(x)
+   y = (input("what is the second number?\n>"))
+   y = int(y)
+   print(str(x) + " / " + str(y) + " = " + str(x / y))
+  
+divide()
---
+++ Unit 2 - Functions and Variables/cal tax.py
@@ -0,0 +1,6 @@
+def calculate_tax(item,price,rate):
+     #print tax
+    print(price * rate) 
+
+calculate_tax("baseball", 4.99, 0.06875)
+    
